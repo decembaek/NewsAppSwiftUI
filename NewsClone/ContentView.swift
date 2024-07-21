49d196474f559274930ca7fe221e3915f087af2b
decembaek <tmdrbpp123@gmail.com>
2024-07-21 15:22:04 +0900

    Initial Commit

---
+++ NewsClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsClone
+//
+//  Created by 백승규 on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsClone/NewsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsCloneApp.swift
+//  NewsClone
+//
+//  Created by 백승규 on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
